59bc617883c576e0e8b5edb3dafe4e56fd6f97ba
fabihelper <fabian.scheuch@live.de>
2020-03-25 16:24:55 +0100

    Aufgabe 5 finished

---
+++ Kapitel_03/src/Aufgabe5/Muster.java
@@ -0,0 +1,20 @@
+package Aufgabe5;
+
+import java.awt.Color;
+
+import javax.swing.*;
+
+public class Muster 
+{
+	public static void main(String[] args)
+	{
+		JFrame mus = new JFrame("Muster");
+		Zeichnung z = new Zeichnung();
+		mus.setContentPane(z);
+		mus.setBackground(Color.white);
+		
+		mus.setSize(1000, 1000);
+		mus.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mus.setVisible(true);
+	}
+}
---
+++ Kapitel_03/src/Aufgabe5/Zeichnung.java
@@ -0,0 +1,38 @@
+package Aufgabe5;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Zeichnung extends JPanel
+{
+	int hoehe = 40*10;
+
+	
+	public void paint(Graphics g)
+	{
+
+			for(int i = 10; i <= hoehe; i=i+10)
+			{
+		
+	            for (int j = 10; j <= i; j=j+10) { 
+	            	
+	            	g.setColor(Color.BLUE);
+	            	g.drawString("+ ", i+510, j); 
+	            	g.drawString("+ ", i*-1+510, j);
+	            	g.setColor(Color.RED);
+	            	g.drawString("* ", j+500, i); 
+	            	g.drawString("* ", j*-1+520, i);
+
+
+	            } 
+				System.out.println();
+			}
+			
+		}
+		
+		
+	
+	
+}
+	
+
